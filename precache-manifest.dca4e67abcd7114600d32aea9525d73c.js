fbe80cb46f67cba105d3a8a68edc534e99fabc35
abbasrizvi24 <rizviabbas24@gmail.com>
2020-07-29 10:58:09 +0500

    Updates

---
+++ precache-manifest.dca4e67abcd7114600d32aea9525d73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a419b15cf00026280d34f8f2c01edefd",
+    "url": "/abbasrizvi24/TODOApp-demo/index.html"
+  },
+  {
+    "revision": "fea12c16ee67f7aac0f8",
+    "url": "/abbasrizvi24/TODOApp-demo/static/css/main.f18a7e82.chunk.css"
+  },
+  {
+    "revision": "7b1a400adaf802834bb8",
+    "url": "/abbasrizvi24/TODOApp-demo/static/js/2.fe99dfe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abbasrizvi24/TODOApp-demo/static/js/2.fe99dfe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea12c16ee67f7aac0f8",
+    "url": "/abbasrizvi24/TODOApp-demo/static/js/main.8552ad70.chunk.js"
+  },
+  {
+    "revision": "63d11f8229447f45b5f7",
+    "url": "/abbasrizvi24/TODOApp-demo/static/js/runtime-main.73e4282e.js"
+  }
+]);
